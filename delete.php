fa18a4dc71a1e1db2bae7cf3544656fba3c6045b
Elissa <elissadusabe@gmail.com>
2023-05-01 18:23:35 +0200

    ended successfully

--- delete.php
+++ delete.php
@@ -4,5 +4,10 @@
    $id = $_GET['id'];
    $sql = "DELETE FROM `crud` WHERE id = $id";
    $result = mysqli_query($conn, $sql);  
+     if($result){
+        header("Location: index.php?msg=Record deleted successfully");
+     } else {
+        echo "Failed: " . mysqli_error($conn);
+     }
 
 ?>
