43a69b8aeee7f5f284f3d2ff759759a344aa9ab4
Elissa <elissadusabe@gmail.com>
2023-05-01 18:15:47 +0200

    added new

--- index.php
+++ index.php
@@ -36,7 +36,29 @@
       <th scope="col">Action</th>
     </tr>
   </thead>
-  
+  <tbody>
+    <?php
+       include "db_conn.php";
+       $sql =  "SELECT * FROM crud ";
+       $result = mysqli_query($conn, $sql);
+       while ($row = mysqli_fetch_assoc($result)){
+    ?> 
+        <tr>
+      <td><?php echo $row['id']?></td>
+      <td><?php echo $row['first_name']?></td>
+      <td><?php echo $row['last_name']?></td>
+      <td><?php echo $row['email']?></td>
+      <td><?php echo $row['gender']?></td>
+      <td>
+       <a href="edit.php?id=<?php echo $row['id'] ?>" class="link_dark"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
+       <a href="delete.php?id=<?php echo $row['id'] ?>" class="link-danger"><i class="fa-solid fa-trash fs-5"></i></a> 
+      </td>
+    </tr> 
+        <?php
+    }
+    ?>
+    
+  </tbody>
 </table>
     </div>
     
