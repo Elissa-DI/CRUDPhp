944c08080ab55ba7107e5d4fc8c394d20dfeb1ba
Elissa <elissadusabe@gmail.com>
2023-05-01 18:22:01 +0200

    debuged edit file

--- edit.php
+++ edit.php
@@ -2,7 +2,11 @@
   include "db_conn.php";
   $id = $_GET['id'];
 
-  
+  if(isset($_POST['submit'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
 
     $sql = "UPDATE `crud` SET `first_name`='$first_name',`last_name`='$last_name',`email`='$email',`gender`='$gender' WHERE id=$id";
     $result = mysqli_query($conn, $sql);
